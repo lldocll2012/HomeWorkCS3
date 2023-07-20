22b92c93c07e4e714bb5ea8468ab6eed0e85124f
lldocll2012 <lldocll2012@outlook.com>
2023-07-20 21:07:37 +0300

    сделал дз

---
+++ Task_19/Program.cs
@@ -0,0 +1,25 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+Console.Clear();
+string Input(string text)
+{
+    Console.WriteLine(text);
+    return Console.ReadLine();
+}
+string a = Input("Enter the number");
+char[] charArray = a.ToCharArray();
+Array.Reverse(charArray);
+string reverseString = new string(charArray);
+
+Console.WriteLine(reverseString);
+
+
+
+if(a == reverseString)
+{
+    Console.WriteLine("Палиндром");
+}
+else
+{
+    Console.WriteLine("Не Палиндром");
+}
+Console.ReadLine();
---
+++ Task_23/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.Clear();
+int Input(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+int a = Input("Pleas enter the number ");
+int count = 1;
+while(count <= a)
+{
+    Console.WriteLine(count * count * count);
+    count++;
+}
